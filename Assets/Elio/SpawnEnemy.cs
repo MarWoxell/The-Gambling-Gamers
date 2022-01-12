8464256636727cab4c939d675abbc4b06cd1cd91
ElioMio <elio.enestrom@elev.ga.lbs.se>
2022-01-12 16:47:22 +0100

    Waves
    Im working on the enemy waves

---
+++ Assets/Elio/SpawnEnemy.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnEnemy : MonoBehaviour
+{
+    public GameObject enemyPrefab;
+    public int[] waveAmount;
+    public int i;
+    public int waveNumber = 0;
+    public static int areThereEnemiesAlive;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    IEnumerator EnemySpawnCooldown()
+    {
+        yield return new WaitForSecondsRealtime(5f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Spawns the enemy according to the amount speciefied in waveAmount with a half second interval
+        for(i = 0; i <= waveAmount[waveNumber]; i++)
+        {
+            if(i == 0)
+            {
+                areThereEnemiesAlive = waveAmount[waveNumber];
+            }
+            EnemySpawnCooldown();
+            SpawnsEnemy();
+            
+        }
+        if (waveAmount[waveNumber] == i && areThereEnemiesAlive == 0)
+        {
+            waveNumber++;
+            i = 0;
+            print("yupyup");
+        }
+    }
+
+    public void SpawnsEnemy()
+    {
+        Instantiate(enemyPrefab);
+    }
+}
