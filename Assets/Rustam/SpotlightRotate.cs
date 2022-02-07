0a0776e0f664995f49cf8de03c2ce520f2d9352e
RustamLBS <rustam.nurgushev@elev.ga.lbs.se>
2022-02-07 11:48:28 +0100

    Bloodsplatter particle

--- Assets/Rustam/SpotlightRotate.cs
+++ Assets/Rustam/SpotlightRotate.cs
@@ -5,11 +5,18 @@ using UnityEngine;
 public class SpotlightRotate : MonoBehaviour
 {
     public Vector3 RotatePoint;
+    public int MinSpeed;
+    public int MaxSpeed;
     public int RotateSpeed;
+    public bool IsNegative;
     // Start is called before the first frame update
     void Start()
     {
-        
+        RotateSpeed = Random.Range(MinSpeed, MaxSpeed);
+        if(IsNegative == true)
+        {
+            RotateSpeed = RotateSpeed * -1;
+        }
     }
 
     // Update is called once per frame
