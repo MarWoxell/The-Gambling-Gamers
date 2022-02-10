a400cb0cb60cf357d0d0885268726586a8cf48f5
Elio Eneström <elio.enestrom@learnet.se>
2022-02-10 10:54:08 +0100

    yep

--- Assets/Elio/Enemy.cs
+++ Assets/Elio/Enemy.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy : MonoBehaviour
+public class Enemy : EnemyAI
 {
     // Start is called before the first frame update
     void Start()
@@ -13,7 +13,7 @@ public class Enemy : MonoBehaviour
     
 
         // Update is called once per frame
-        void Update()
+    void Update()
     {
         
     }
